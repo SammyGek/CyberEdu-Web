db8d8e0fbb6e851cbdc86e53f40886bee8cb3e61
SammyGek <samumarcadiz@gmail.com>
2026-01-07 17:26:14 +0100

    comentario fix

--- assets/supbase-config.js
+++ assets/supbase-config.js
@@ -1,3 +1,3 @@
-// CyberEdu - Supabase Configuration
+// Hakiu - Supabase Configuration
 window.SUPABASE_URL = 'https://nkzwowmqsqrousjqycjn.supabase.co';  
 window.SUPABASE_ANON_KEY = 'sb_publishable_po8-zsWBHKeACQgPLHhUig_rxyaIR4h';            
